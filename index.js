71aca51a25d2f4e7561d83504519f9bf86768414
Roger Vasconcelos Santana <rogerdev.contato@gmail.com>
2023-09-13 13:06:56 -0300

    Refatorando os métodos POST(CREATE) e GET(READ), incluindo o método DELETE e realizando um  melhor ajuste das classes no CSS

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const form = document.querySelector('form')
 
-form.addEventListener('submit', (e) => {
+form.addEventListener('submit', async (e) => {
     e.preventDefault()
 
     const formData = new FormData(form)
     const dadosForm = Object.fromEntries(formData)
 
-    adicionaUser(dadosForm.nome, dadosForm.email)
+    await postDados(dadosForm)
+    await removerDados()
 
     const inputForm = document.querySelectorAll('form input')
     inputForm.forEach( input => {
@@ -14,7 +15,21 @@ form.addEventListener('submit', (e) => {
     })
 })
 
-const adicionaUser = (nome, email) => {
+const getDados = async () => {
+    let url = 'https://reqres.in/api/users?page=2'
+
+    await fetch(url)
+        .then(resposta => resposta.json())
+        .then(dados => {
+            dados.data.forEach(dado => {
+                adicionaUser(dado.id, dado.first_name, dado.email)
+            })
+        })
+}
+
+getDados()
+
+const adicionaUser = (id, nome, email) => {
 
     const divImgUser = document.createElement('div')
     const imgUser = document.createElement('img')
@@ -24,6 +39,7 @@ const adicionaUser = (nome, email) => {
     divImgUser.appendChild(imgUser)
 
     const divDadosUser = document.createElement('div')
+    divDadosUser.classList.add('divDadosUser')
     const h3Nome = document.createElement('h3')
     h3Nome.innerHTML = nome
     const spanEmail = document.createElement('span')
@@ -40,29 +56,60 @@ const adicionaUser = (nome, email) => {
 
     divCloseBtn.appendChild(closeBtn)
 
+    const divUsuario = document.createElement('div')
+    divUsuario.setAttribute('dataIdUser', id)
+    divUsuario.classList.add('usuario')
 
-    const divUsuarios = document.createElement('div')
-    divUsuarios.classList.add('usuarios')
 
-    divUsuarios.appendChild(divImgUser)
-    divUsuarios.appendChild(divDadosUser)
-    divUsuarios.appendChild(divCloseBtn)
+    divUsuario.appendChild(divImgUser)
+    divUsuario.appendChild(divDadosUser)
+    divUsuario.appendChild(divCloseBtn)
 
-    document.querySelector('main').appendChild(divUsuarios)
+    const divUsuarios = document.querySelector('div.usuarios')
+
+
+    divUsuarios.appendChild(divUsuario)
 
 }
 
-
-const getDadosAPI = () => {
-
+const postDados = async (dadosForm) => {
     let url = 'https://reqres.in/api/users?page=2'
-    fetch(url)
+
+    await fetch(url, {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(dadosForm)
+    })
         .then(resposta => resposta.json())
-        .then(dados => {
-            dados.data.map( dado => {
-                adicionaUser(dado.first_name, dado.email)
-            })
+        .then(dados =>{
+            adicionaUser(dados.id, dados.nome, dados.email)
         })
+
 }
 
-getDadosAPI()
+const removerDados = async () => {
+
+
+    document.querySelectorAll('.divCloseBtn').forEach( btn => {
+
+        btn.addEventListener('click', async (e) => {
+            if(window.confirm('Tem certaza que deseja excluir este registro?')){
+                if(e.target.classList.contains('closeBtn')){
+
+                    const userExcluido = e.target.closest('.usuario')
+                    const id = userExcluido.getAttribute('dataiduser')
+
+                    let url =`https://reqres.in/api/users/${id}`
+                    await fetch(url, {
+                        method: 'DELETE',
+                    }).then( resposta => {
+                        if(resposta.ok){
+                            console.log('click')
+                            userExcluido.remove()
+                        }
+                    })
+                }
+            }
+        })
+    })
+}
